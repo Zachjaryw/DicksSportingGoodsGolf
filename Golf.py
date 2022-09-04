d885f6c307e487c9960dd8e8dd76a5e504c984f3
Zachjaryw <85905928+Zachjaryw@users.noreply.github.com>
2022-09-04 11:35:02 -0700

    Update Golf.py

--- Golf.py
+++ Golf.py
@@ -1,6 +1,19 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
+from Dropbox_Setup import *
 
 st.title('Dicks Sporting Goods Golf Department')
 
+dbx = initializeToken(st.secrets.dropbox.access)
+
+toDBX(dbx,st.secrets.filepath.barcode,{'Barcode':[],
+                          'Brand':[],
+                          'Club Type':[],
+                          'Specifics':[]})
+toDBX(dbx,st.secrets.filepath.clubID,{'Serial Code':[],
+                                     'Barcode':[],
+                                     'Status':[]})
+
+barcodes = fromDBX(dbx,st.secrets.filepath.barcode)
+clubID = fromDBX(dbx,st.secrets.filepath.clubID)
