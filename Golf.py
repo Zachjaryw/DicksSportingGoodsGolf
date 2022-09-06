421ed8613dca84e2e8b92e49c32eae5d41a9b6c0
Zachjaryw <85905928+Zachjaryw@users.noreply.github.com>
2022-09-05 20:58:01 -0700

    Update Golf.py

--- Golf.py
+++ Golf.py
@@ -8,9 +8,6 @@ st.title('Golf Club Inventory System')
 
 dbx = initializeToken(st.secrets.dropbox.access)
 
-toDBX(dbx,{'Barcode':[],'Brand':[],'Club Type':[],'Number':[],'Specifics':[]},st.secrets.filepath.barcode)
-toDBX(dbx,{'Serial Code':[],'Barcode':[],'Status':[]},st.secrets.filepath.clubID)
-
 barcodes = fromDBX(dbx,st.secrets.filepath.barcode)
 clubID = fromDBX(dbx,st.secrets.filepath.clubID)
 
