2f52688cd631da5734884f177b0012c3c00fde23
Zachjaryw <85905928+Zachjaryw@users.noreply.github.com>
2022-09-07 22:20:55 -0700

    Update Golf.py

--- Golf.py
+++ Golf.py
@@ -11,7 +11,11 @@ dbx = initializeToken(st.secrets.dropbox.access)
 barcodes = fromDBX(dbx,st.secrets.filepath.barcode)
 clubID = fromDBX(dbx,st.secrets.filepath.clubID)
 
-st.write(barcodes['Barcode'][0])
+barcodes['Number'][0] = '10.5 Degree'
+barcodes['Specifics'][0] = 'Mavrik'
+st.write(barcodes)
+if st.button('submit'):
+  toDBX(dbx,barcodes,st.secrets.filepath.barcode)
 
 add,data,reset,save = st.tabs(['Add, adjust, or check a club by barcode and serial code','Review Data','Reset Data','Save Data'])
 
