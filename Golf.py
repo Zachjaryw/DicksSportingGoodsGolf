2213b4595acf9034e8fd21a672e01d72d3878564
Zachjaryw <85905928+Zachjaryw@users.noreply.github.com>
2022-09-07 22:11:51 -0700

    Update Golf.py

--- Golf.py
+++ Golf.py
@@ -10,9 +10,9 @@ dbx = initializeToken(st.secrets.dropbox.access)
 
 barcodes = fromDBX(dbx,st.secrets.filepath.barcode)
 clubID = fromDBX(dbx,st.secrets.filepath.clubID)
-a = clubID['Serial Code'].values.tolist()
-b = clubID['Barcode'].values.tolist()
-c = clubID['Status'].values.tolist()
+a = clubID['Serial Code'].values
+b = clubID['Barcode'].values
+c = clubID['Status'].values
 club = {'Serial Code':a,'Barcode':b,'Status':c}
 st.write(club)
 if st.button('confrifn'):
